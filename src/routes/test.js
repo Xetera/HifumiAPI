bbca8bad95bef60466667e90204c9fa1258f40eb
Unknown <mythologyali@gmail.com>
2018-06-15 13:10:59 -0700

    removed js files

--- src/routes/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.testEndpoint = function (router) {
-    router.get('/test', function (req, res, next) {
-        res.send("You're in homie.");
-    });
-};
