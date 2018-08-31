d014e64bb02325d3a3e800bf8521039e20cdaee1
Unknown <mythologyali@gmail.com>
2018-08-31 15:02:59 -0700

    fixed ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "HifumiAPI",
+      script: "dist/index.js",
+      max_restarts: 3,
+      env: {
+        NODE_ENV: "DEVELOPMENT"
+      },
+      env_production: {
+        NODE_ENV: "PRODUCTION"
+      }
+    }
+  ]
+};
