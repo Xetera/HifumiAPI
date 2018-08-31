4d9de26b5be44e9113226d6a144bdf5885711b16
Unknown <mythologyali@gmail.com>
2018-08-31 16:12:58 -0700

    removed unused features and deps

--- src/modules/chat.js
+++ src/modules/chat.js
@@ -1,24 +1,24 @@
-import Nightmare from 'nightmare';
+// import Nightmare from 'nightmare';
 
-const nightmare = new Nightmare({ show: true, typeInterval: 1 })
-	.goto('https://www.cleverbot.com/');
+// const nightmare = new Nightmare({ show: true, typeInterval: 1 })
+// 	.goto('https://www.cleverbot.com/');
 
-const say = (query) => {
-	console.log('got a request for say');
-	return nightmare
-		.type('.stimulus', query)
-		.click('.sayitbutton')
-		.evaluate((text, done) => {
-			const existCondition = setInterval(() => {
-				// eslint-disable-next-line
-				if (document.querySelector('#line1 > .yellow').style.opacity === '1') {
-					clearInterval(existCondition);
+// const say = (query) => {
+// 	console.log('got a request for say');
+// 	return nightmare
+// 		.type('.stimulus', query)
+// 		.click('.sayitbutton')
+// 		.evaluate((text, done) => {
+// 			const existCondition = setInterval(() => {
+// 				// eslint-disable-next-line
+// 				if (document.querySelector('#line1 > .yellow').style.opacity === '1') {
+// 					clearInterval(existCondition);
 
-					// eslint-disable-next-line
-					done(null, document.querySelector('#line1 > .bot').innerText);
-				}
-			}, 100);
-		}, query);
-};
+// 					// eslint-disable-next-line
+// 					done(null, document.querySelector('#line1 > .bot').innerText);
+// 				}
+// 			}, 100);
+// 		}, query);
+// };
 
-module.exports = { say };
+// module.exports = { say };
